1f7339781d503f669f1bbc88c3a75bc9597887d3
ssjivan <ssjivan@e4391b25-1b53-0410-94bc-f36b45c2e315>
2008-09-28 18:59:13 +0000

    Adaptada la clase Resultado, para que vaya funcionando con el And/Or/Not ya implementados.
    Aqui añadimos la nueva clase Visitante, que es el que se encarga de visitar las Formulas. Este posee el metodo de visit().

--- trunk/src/ucm/si/Checker/Resultado.java
+++ trunk/src/ucm/si/Checker/Resultado.java
@@ -11,37 +11,32 @@ import java.util.List;
 // </editor-fold> 
 public class Resultado {
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.19A0C54A-5417-6654-88F9-BD1D26871F5C]
-    // </editor-fold> 
-    public static final int cierto = 0;
-
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.0493DD25-45B0-3444-F17A-F3F9B10A082D]
-    // </editor-fold> 
-    public static final int falso = 1;
-
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.C71DB725-CBD4-0D46-FD98-E7ABE22A07F2]
-    // </editor-fold> 
-    public static final int indeterminado = -1;
+	public static final String COD_TRUE= "true";
+	public static final String COD_FALSE= "false";
+	public static final String COD_MAYBE= "maybe";
+	public static final String COD_ERROR= "error";
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.1203B296-E7C4-00BB-828B-EEEB38B50A65]
     // </editor-fold> 
-    private int solucion;
+    private String resultado=null;
 
+    public Resultado(String tipo){
+		this.resultado= tipo;
+	}
+    
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.1A8EBBF6-85AB-1659-453B-146B69450E7D]
     // </editor-fold> 
     private List<Estado> contraejemplo;
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.790D13C2-FBAB-6557-5DD8-112DC177EA1C]
-    // </editor-fold> 
-    public int getSolucion () {
-        return 0;
-    }
+	public String getResultado() {
+		return resultado;
+	}
+
+	public void setResultado(String resultado) {
+		this.resultado = resultado;
+	}
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.0DF466D2-73FF-53F9-0248-7028D2277309]
@@ -56,11 +51,6 @@ public class Resultado {
     public void setContraejemplo (List<Estado> contraejemplo) {
     }
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.3B740220-56D3-173F-C6A7-0A6D34DCB6A4]
-    // </editor-fold> 
-    public void setSolucion (int solucion) {
-    }
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.A0BCDA9B-E166-AFE8-2706-24416EF66A02]
