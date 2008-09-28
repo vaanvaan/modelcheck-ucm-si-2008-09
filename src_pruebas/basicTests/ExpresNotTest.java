13ed7bfc703a04254f3ba8874aca82beaf97c0ff
ssjivan <ssjivan@e4391b25-1b53-0410-94bc-f36b45c2e315>
2008-09-28 18:52:45 +0000

    Estos son unos test básicos sobre la evaluación de fórmulas. Además en esta carpeta vamos a añadir pruebas, JUnit tests para probar las diferentes funcionalidades de la aplicación.
    Esta primera versión sólo hace un test a formulas constantes con operadores logicos. And/Or/Not.

---
+++ src_pruebas/basicTests/ExpresNotTest.java
@@ -0,0 +1,38 @@
+package basicTests;
+
+import ucm.si.Checker.Resultado;
+import ucm.si.Checker.Visitante;
+import ucm.si.basico.ecuaciones.And;
+import ucm.si.basico.ecuaciones.Formula;
+import ucm.si.basico.ecuaciones.Not;
+import ucm.si.basico.ecuaciones.Or;
+import ucm.si.basico.ecuaciones.Proposicion;
+import junit.framework.TestCase;
+
+
+public class ExpresNotTest extends TestCase{
+
+	public void test2Not() throws Exception {
+	    Visitante visitante = new Visitante();
+	    Formula ctlexp = new Not(new Not(new Proposicion(Resultado.COD_FALSE)));
+		visitante.visita(ctlexp);
+		System.out.println(visitante.getResParcial().getResultado());
+	    assertEquals(Resultado.COD_FALSE, visitante.getResParcial().getResultado());
+	}
+	
+	public void testAnd()throws Exception{
+		Visitante visitante = new Visitante();
+		Formula ctlexp = new And(new Not(new Proposicion(Resultado.COD_FALSE)),new Proposicion(Resultado.COD_TRUE));
+		visitante.visita(ctlexp);
+		System.out.println(visitante.getResParcial().getResultado());
+	    assertEquals(Resultado.COD_TRUE, visitante.getResParcial().getResultado());
+	}
+	
+	public void testOr() throws Exception{
+		Visitante visitante = new Visitante();
+		Formula ctlexp = new Or(new Proposicion(Resultado.COD_MAYBE),new Not(new Proposicion(Resultado.COD_FALSE)));
+		visitante.visita(ctlexp);
+		System.out.println(visitante.getResParcial().getResultado());
+		assertEquals(Resultado.COD_TRUE, visitante.getResParcial().getResultado());
+	}
+}
