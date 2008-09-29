86791e7944df8a495e46b8a914f9d638c0684a27
ssjivan <ssjivan@e4391b25-1b53-0410-94bc-f36b45c2e315>
2008-09-29 20:32:35 +0000

    Cambios: El visitante incorpora la chicha de las formulas logicas basicas. Ahora tiene un metodo visita para cada operacion logica (de momento sólo and/or/not.

--- src/ucm/si/Checker/Visitante.java
+++ src/ucm/si/Checker/Visitante.java
@@ -1,16 +1,28 @@
 package ucm.si.Checker;
 
+import java.util.Stack;
+
+import ucm.si.basico.ecuaciones.And;
 import ucm.si.basico.ecuaciones.Formula;
+import ucm.si.basico.ecuaciones.Not;
+import ucm.si.basico.ecuaciones.Or;
 
 
 // añadir constructora para usar logs globales, si es necesario.
 public  class Visitante {
 	
 	private Resultado resParcial = null;
+	private Stack<Formula> pilaFormulas = new Stack<Formula>();
+	private boolean inicio = false;
 	
 	public Resultado visita(Formula expresion){
-		Formula exp = expresion;
-		exp.accept(this);
+		expresion.accept(this);
+		/*while (!pilaFormulas.isEmpty())
+		{ 
+			Formula e = pilaFormulas.pop();
+			e.accept(this);
+		}
+		*/
 		//actualizo el resultado parcial
 		return resParcial;
 	}
@@ -21,5 +33,60 @@ public  class Visitante {
 
 	public void setResParcial(Resultado resParcial) {
 		this.resParcial = resParcial;
+	}
+	public void visita(Not n){
+		n.getFormula().accept(this);
+		if (resParcial.equals(Resultado.COD_TRUE)){
+			resParcial.setResultado(Resultado.COD_FALSE);
+		} else if (resParcial.equals(Resultado.COD_FALSE)){
+			resParcial.setResultado(Resultado.COD_TRUE);
+		}else if (resParcial.equals(Resultado.COD_MAYBEF))
+			resParcial.setResultado(Resultado.COD_MAYBET);
+		else resParcial.setResultado(Resultado.COD_MAYBEF);		
+	}
+	public void visita(Or or){
+		or.getExpIzq().accept(this);
+		Resultado resIzq = resParcial;
+		or.getExpDer().accept(this);
+		Resultado resDer = resParcial;
+		Resultado resAND;
+		try{
+			boolean part1 = Boolean.parseBoolean(resIzq.getResultado());
+			boolean part2 = Boolean.parseBoolean(resDer.getResultado());
+			part1 = part1 || part2;
+			resAND = new Resultado(String.valueOf(part1));
+		}catch(Exception e){
+			if((resIzq.getResultado().equals(Resultado.COD_TRUE)) ||
+					(resDer.getResultado().equals(Resultado.COD_TRUE)) ){
+				resAND = new Resultado(Resultado.COD_TRUE);
+			}else{
+				resAND = new Resultado(Resultado.COD_MAYBET);
+			}
+		}
+		resParcial=resAND;
+	}
+	public void visita(And and){
+
+		// TODO Auto-generated method stub
+		and.getExpIzq().accept(this);
+		Resultado resIzq = resParcial;
+		and.getExpDer().accept(this);
+		Resultado resDer = resParcial;
+		Resultado resAND;
+		try{
+			boolean part1 = Boolean.parseBoolean(resIzq.getResultado());
+			boolean part2 = Boolean.parseBoolean(resDer.getResultado());
+			part1 = part1 && part2;
+			resAND = new Resultado(String.valueOf(part1));
+		}catch(Exception e){
+			if((resIzq.getResultado().equals(Resultado.COD_FALSE)) ||
+					(resDer.getResultado().equals(Resultado.COD_FALSE)) ){
+				resAND = new Resultado(Resultado.COD_FALSE);
+			}else{
+				resAND = new Resultado(Resultado.COD_MAYBEF);
+			}
+		}
+		resParcial=resAND;
+	
 	}
 }
