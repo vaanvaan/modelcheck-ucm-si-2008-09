d0d89665d58544dec0795b39bf1cc4b1c63de963
nicolas.mon.trotti <nicolas.mon.trotti@e4391b25-1b53-0410-94bc-f36b45c2e315>
2008-09-30 15:25:42 +0000

    Estado parametrizado

--- trunk/src/ucm/si/Checker/ModelChecker.java
+++ trunk/src/ucm/si/Checker/ModelChecker.java
@@ -5,17 +5,17 @@ import ucm.si.basico.ecuaciones.Formula;
 // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
 // #[regen=yes,id=DCE.21B8A663-FA11-A9B0-67A5-53B60CD8727B]
 // </editor-fold> 
-public interface ModelChecker {
+public interface ModelChecker<S> {
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.7976A13B-E5F7-6A8F-1AD3-4E19D7E8B67B]
     // </editor-fold> 
-    public Resultado chequear (Interprete interprete, Formula proposicion);
+    public Resultado chequear (Interprete<S> interprete, Formula formula);
 
     // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
     // #[regen=yes,id=DCE.4CABA78F-38BB-549A-D70B-BD44F2FE74CB]
     // </editor-fold> 
-    public Resultado chequear (Interprete interprete, Formula proposicion, Estado estado);
+    public Resultado chequear (Interprete<S> interprete, Formula formula, S estado);
 
 }
 
