5f0953a82d16c086c683c6047ab32436a853f6c9
nicolas.mon.trotti <nicolas.mon.trotti@e4391b25-1b53-0410-94bc-f36b45c2e315>
2008-09-30 16:19:05 +0000

    Estado parametrizado

--- src/ucm/si/Checker/InterpreteWraper.java
+++ src/ucm/si/Checker/InterpreteWraper.java
@@ -5,12 +5,39 @@
 
 package ucm.si.Checker;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author nico
  */
-public class InterpreteWraper <I>{
+public class InterpreteWraper {
     
-    private 
+    private String nombre;
+    private Interprete interprete;
+
+    public InterpreteWraper(Interprete interprete) {
+        this.interprete = interprete;
+        this.nombre = this.interprete.getClass().getName();
+    }
+
+    public InterpreteWraper(String nombre) {
+        try {
+            this.nombre = nombre;
+        
+            this.interprete = (Interprete) Class.forName(this.nombre).newInstance();
+        } catch (InstantiationException ex) {
+            Logger.getLogger(InterpreteWraper.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(InterpreteWraper.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(InterpreteWraper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    
+    
+    
     
 }
