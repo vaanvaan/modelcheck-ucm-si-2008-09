2373a38bab3f94a7bc04e7aacd4c833afe25db94
nicolas.mon.trotti <nicolas.mon.trotti@e4391b25-1b53-0410-94bc-f36b45c2e315>
2008-09-30 16:00:48 +0000

    Estado parametrizado

---
+++ src/ucm/si/Checker/InterpreteWraper.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ucm.si.Checker;
+
+/**
+ *
+ * @author nico
+ */
+public class InterpreteWraper <I>{
+    
+    private 
+    
+}
