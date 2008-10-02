24b4c6f6308c9a4c06a93de588010d319a20f8de
ssjivan <ssjivan@e4391b25-1b53-0410-94bc-f36b45c2e315>
2008-10-02 21:29:40 +0000

    Uff esto es para poder usar las proposiciones. Mira que se vuelve complicado esto, no?

---
+++ src/ucm/si/Laberinto/LaberintoPropo.java
@@ -0,0 +1,26 @@
+package ucm.si.Laberinto;
+
+import ucm.si.Checker.Interprete;
+import ucm.si.basico.ecuaciones.Proposicion;
+
+public class LaberintoPropo extends Proposicion<Posicion>{
+	private Laberinto lab;
+	private Posicion p;
+	
+	public LaberintoPropo(Posicion p){
+		this.p = p;
+	}
+	
+	public Laberinto getLab() {
+		return lab;
+	}
+	public void setLab(Interprete<Posicion> lab2) {
+		this.lab = (Laberinto) lab2;
+	}
+	@Override
+	public boolean esCierta(Posicion s) {
+		lab.checkPos(s);
+		return false;
+	}
+
+}
