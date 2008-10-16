b2d0a2159baa48ad95a91eae214166364e9eaf8c
nicolas.mon.trotti <nicolas.mon.trotti@e4391b25-1b53-0410-94bc-f36b45c2e315>
2008-10-16 16:42:14 +0000

    Version con arbol. Los caminos estan implementados con arboles. Las funciones AU y EU solo devuelven el arbol correspondiente a sus formulas.

---
+++ src_aux/ucm/si/basico/ecuaciones/EX.java
@@ -0,0 +1,21 @@
+package ucm.si.basico.ecuaciones;
+
+import ucm.si.Checker.ModelChecker; 
+import ucm.si.Checker.Visitante;
+
+// <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+// #[regen=yes,id=DCE.709D66FA-53B6-D1F5-0872-724E95CC588D]
+// </editor-fold> 
+public class EX extends OperacionUnaria {
+
+    public EX(Formula f) {
+        super(f);
+    }
+
+    @Override
+    public void accept(Visitante v) {
+        v.visita(this);
+    }
+    
+}
+
