1f19e75962e39e5f04325faf3d70ab687eba41eb
ssjivan <ssjivan@e4391b25-1b53-0410-94bc-f36b45c2e315>
2008-11-04 21:23:46 +0000

    Sketch de animador, sólo se pinta y hace avanzar, pero no refresca, estoy en ello.

---
+++ trunk/src/ucm/si/navegador/FrameAnimador.java
@@ -0,0 +1,195 @@
+package ucm.si.navegador;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.SwingConstants;
+
+import ucm.si.Laberinto.Laberinto;
+import ucm.si.Laberinto.Posicion;
+import ucm.si.navegador.events.Avanza;
+
+public class FrameAnimador<S> extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Navegador<S> nav;
+	private JPanel lienzo;
+	private S estadoactual;
+	private Laberinto lab;
+
+	public S getEstadoactual() {
+		return estadoactual;
+	}
+
+	public void setEstadoactual(S estadoactual) {
+		this.estadoactual = estadoactual;
+	}
+
+	public JPanel getLienzo() {
+		return lienzo;
+	}
+
+	public void setLienzo(JPanel lienzo) {
+		this.lienzo = lienzo;
+	}
+
+	public FrameAnimador(final AnimadorFrame control) {
+		// Estado S;
+		this.setTitle("Animador v1");
+		ActionListener actionListener = new ActionListener() {
+			public void actionPerformed(ActionEvent actionEvent) {
+				FrameAnimador.this.dispose();
+			}
+		};
+		ActionListener actionListenerAvanzar = new ActionListener() {
+			public void actionPerformed(ActionEvent actionEvent) {
+				control.aplicaAvanza();
+			}
+		};
+		ActionListener actionListenerGo_to = new ActionListener() {
+			public void actionPerformed(ActionEvent actionEvent) {
+
+			}
+		};
+		ActionListener actionListenerRetroceder = new ActionListener() {
+			public void actionPerformed(ActionEvent actionEvent) {
+
+			}
+		};
+		lab = control.getLab();
+		estadoactual = (S) control.getEstadoactual();
+		JPanel pane = new JPanel();
+		pane.setLayout(new GridLayout());
+		JButton boton = new JButton("Cerrar");
+		JButton boton1 = new JButton("Avanzar");
+		JButton boton2 = new JButton("Go To");
+		JButton boton3 = new JButton("Retroceder");
+		lienzo = pintaEstado();
+		lienzo.setSize(200, 200);
+		boton1.setHorizontalTextPosition(SwingConstants.CENTER);
+		boton2.setHorizontalTextPosition(SwingConstants.CENTER);
+		boton3.setHorizontalTextPosition(SwingConstants.CENTER);
+		boton1.addActionListener(actionListenerAvanzar);
+		boton2.addActionListener(actionListenerGo_to);
+		boton3.addActionListener(actionListenerRetroceder);
+		boton.addActionListener(actionListener);
+		pane.add(lienzo);
+		pane.add(boton1);
+		pane.add(boton2);
+		pane.add(boton3);
+		Container c = this.getContentPane();
+		c.setLayout(new BorderLayout());
+		c.add(boton, BorderLayout.NORTH);
+		c.add(lienzo, BorderLayout.CENTER);
+		this.getContentPane().add(new JScrollPane(pane), BorderLayout.SOUTH);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(400, 400);
+		this.setVisible(true);
+
+	}
+
+	public Navegador<S> getNav() {
+		return nav;
+	}
+
+	public void setNav(Navegador<S> nav) {
+		this.nav = nav;
+	}
+
+	public JPanel pintaEstado() {
+		/*
+		 * Pintamos el laberinto.
+		 */
+		ImageIcon aguaIcon = new ImageIcon("etc/agua.jpg");
+		ImageIcon hierbaIcon = new ImageIcon("etc/hierba.jpg");
+		ImageIcon caballeroIcon = new ImageIcon("etc/caballero.jpg");
+
+		Posicion p = (Posicion) estadoactual;
+
+		JPanel estado = new JPanel();
+		GridLayout cuadrados = new GridLayout(lab.getDim(), lab.getDim());
+		// JButton boton = new JButton();
+		cuadrados.setColumns(lab.getDim());
+		cuadrados.setHgap(lab.getDim());
+		// Image imagenFuente =
+		// Toolkit.getDefaultToolkit().getImage("agua.jpg");
+
+		estado.setLayout(cuadrados);
+
+		for (int i = 0; i < lab.getDim(); i++) {
+			for (int j = 0; j < lab.getDim(); j++) {
+				if (p.getPosX() == i && p.getPosY() == j) {
+					JButton jb = new JButton();
+					jb.setName("b" + i + "," + j);
+					jb.setIcon(caballeroIcon);
+					estado.add(jb);
+				} else {
+					if (lab.checkPos(new Posicion(i, j))) {
+						JButton jb = new JButton();
+						jb.setName("b" + i + "," + j);
+						jb.setIcon(hierbaIcon);
+						estado.add(jb);
+					} else {
+						JButton jb = new JButton();
+						jb.setName("b" + i + "," + j);
+						jb.setIcon(aguaIcon);
+						estado.add(jb);
+					}
+				}
+			}
+		}
+		return estado;
+	}
+
+	public void rePinta() {
+		ImageIcon aguaIcon = new ImageIcon("etc/agua.jpg");
+		ImageIcon hierbaIcon = new ImageIcon("etc/hierba.jpg");
+		ImageIcon caballeroIcon = new ImageIcon("etc/caballero.jpg");
+
+		Posicion p = (Posicion) estadoactual;
+
+		lienzo = new JPanel();
+		GridLayout cuadrados = new GridLayout(lab.getDim(), lab.getDim());
+		cuadrados.setColumns(lab.getDim());
+		cuadrados.setHgap(lab.getDim());
+
+		lienzo.setLayout(cuadrados);
+
+		for (int i = 0; i < lab.getDim(); i++) {
+			for (int j = 0; j < lab.getDim(); j++) {
+				if (p.getPosX() == i && p.getPosY() == j) {
+					JButton jb = new JButton();
+					jb.setName("b" + i + "," + j);
+					jb.setIcon(caballeroIcon);
+					lienzo.add(jb);
+				} else {
+					if (lab.checkPos(new Posicion(i, j))) {
+						JButton jb = new JButton();
+						jb.setName("b" + i + "," + j);
+						jb.setIcon(hierbaIcon);
+						lienzo.add(jb);
+					} else {
+						JButton jb = new JButton();
+						jb.setName("b" + i + "," + j);
+						jb.setIcon(aguaIcon);
+						lienzo.add(jb);
+					}
+				}
+			}
+		}
+		lienzo.repaint();
+	}
+
+}
