1c041838b46854d4b52569cf29cb55de4b22b305
josanton <josanton@e4391b25-1b53-0410-94bc-f36b45c2e315>
2008-11-16 15:15:51 +0000

    resueltos fallos en anterior subida

---
+++ trunk/src/ucm/si/Checker/util/Roseta.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ucm.si.Checker.util;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ *
+ * @author Pilar
+ */
+public class Roseta<S> extends HashMap<S, List<StateAndLabel<S>>>{
+
+}
