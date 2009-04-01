d1963dc5ba04752152242b56eec44245ed9218ba
nicolas.mon.trotti <nicolas.mon.trotti@e4391b25-1b53-0410-94bc-f36b45c2e315>
2009-04-01 23:54:06 +0000

    generacion de conjuntos conflictivos funciona.

--- trunk/src/ucm/si/TeoriaActividad/Interprete/Pruebas.java
+++ trunk/src/ucm/si/TeoriaActividad/Interprete/Pruebas.java
@@ -7,6 +7,7 @@ package ucm.si.TeoriaActividad.Interprete;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -34,24 +35,34 @@ public class Pruebas
         Item item1 = new Item("1");
         Item item2 = new Item("2");
         Item item3 = new Item("3");
+        Item item4 = new Item("4");
+        Item item5 = new Item("5");
+        Item item6 = new Item("6");        
         ItemGenerator itemGen = ItemGenerator.getReference();
-        Item[] listaItem1 = {item1, item2};
-        Item[] listaItem2 = {item2, item3};
+        Item[] listaItem1 = {item1, item2, item3};
+        Item[] listaItem2 = {item1, item2, item4};
+        Item[] listaItem3 = {item1, item3, item5};
+        
         Actividad actividad1 = new Actividad("A1", listaItem1, null, null, null);
         Actividad actividad2 = new Actividad("A2", listaItem2, null, null, null);
+        Actividad actividad3 = new Actividad("A3", listaItem3, null, null, null);
         ActividadGenerator activGen = ActividadGenerator.getReference();
         itemGen.addItem(item1);
         itemGen.addItem(item2);
         itemGen.addItem(item3);
+        itemGen.addItem(item4);
+        itemGen.addItem(item5);
+        itemGen.addItem(item6);
         activGen.addActividad(actividad1);
         activGen.addActividad(actividad2);
+        activGen.addActividad(actividad3);
         
         // Creamos la tabla para particionar los items en subconjuntos
         // por ahora hacemos para 32 actividades, pero es facil hacerlo generico
         
         String[] lItems = itemGen.getItems();
         int[] lClaves = new int[lItems.length];
-        Actividad[] lacts = activGen.getConjunto().keySet().
+        Actividad[] lacts = activGen.getConjunto().values().
                 toArray(new Actividad[activGen.Elements()]);
         for (int j = 0; j < lItems.length; j++){
             if (lacts[0].necesita(itemGen.getItem(lItems[j]))){
@@ -60,11 +71,13 @@ public class Pruebas
                 lClaves[j] = 0;
             }
         }
-        for (int i = 0; i < lacts.length; i++){
+        int pot2 = 2;
+        for (int i = 1; i < lacts.length; i++){
             for (int j =0; j < lItems.length; j++){
                 if (lacts[i].necesita(itemGen.getItem(lItems[j])))
-                    lClaves[j] = lClaves[j] + 2^i;
+                    lClaves[j] = lClaves[j] + pot2;
             }
+            pot2 = pot2 << 1;
         }
         HashMap<Integer,Set<String>> conjuntosItems = new HashMap<Integer,Set<String>>();
         for (int i=0; i < lClaves.length; i++){
@@ -78,19 +91,57 @@ public class Pruebas
         }
         
         // Ahora nos quedamos solo con los conjuntos conflictivos
-        for (int i = 0; i < lacts.length+1; i++) {
-            Integer clave = new Integer(2^i);
+        if (conjuntosItems.containsKey(new Integer(0))){
+                conjuntosItems.remove(new Integer(0));
+        }
+        pot2 = 1;
+        for (int i = 0; i < lacts.length; i++) {
+            Integer clave = new Integer(pot2);
             if (conjuntosItems.containsKey(clave)){
                 conjuntosItems.remove(clave);
             }
+            pot2 = pot2<<1;
         }
-        
-        PseudoEstado estadoIni = new PseudoEstado();
+        HashMap<Integer,Set<Actividad>> conjuntosActividades = new HashMap<Integer,Set<Actividad>>();
+        for (Iterator<Integer> it = conjuntosItems.keySet().iterator(); it.hasNext();) {
+            Integer i = it.next();
+            String[] lString = conjuntosItems.get(i).toArray(new String[0]);            
+            pot2=1;
+            for (int j=1;j<=lacts.length;j++){
+                if ((pot2&i)>=1){
+                    if (!conjuntosActividades.containsKey(i)){
+                        HashSet<Actividad> hashaux = new HashSet<Actividad>();
+                        hashaux.add(lacts[j-1]);
+                        conjuntosActividades.put(i,hashaux);
+                    } else {
+                        conjuntosActividades.get(i).add(lacts[j-1]);
+                    }
+                }
+                pot2 = pot2<<1;
+            }
+            StringBuffer strbuf = new StringBuffer("Grupo items ");
+            strbuf.append(i);
+            strbuf.append(": ");
+            strbuf.append(lString[0]);
+            for (int k = 1; k < lString.length;k++){
+                strbuf.append(',');
+                strbuf.append(lString[k]);
+            }
+            strbuf.append(" => ");
+            for (Iterator<Actividad> it1 = conjuntosActividades.get(i).iterator(); it1.hasNext();) {
+                Actividad a = it1.next();
+                strbuf.append(a.getNombre());
+                strbuf.append(',');
+            }
+            strbuf.deleteCharAt(strbuf.length()-1);
+            System.out.println(strbuf.toString());
+        }
+        /*PseudoEstado estadoIni = new PseudoEstado();
         estadoIni.actividades = new ListaEstadosActividades();
         estadoIni.items = new ListaEstadosItems();
 
 
-        backtracking(estadoIni);
+        backtracking(estadoIni);*/
 
 
 
