7e1e1c03e39315197c05a1bb52b6ed1be49713d7
Nicolas.Mon.Trotti <Nicolas.Mon.Trotti@e4391b25-1b53-0410-94bc-f36b45c2e315>
2009-05-05 04:57:48 +0000

    Haciendo ejemplo RCP.

--- src_estable/ucm/si/TeoriaActividad/Interprete/SistemaActividades.java
+++ src_estable/ucm/si/TeoriaActividad/Interprete/SistemaActividades.java
@@ -27,48 +27,17 @@ import ucm.si.TeoriaActividad.item.*;
  */
 public class SistemaActividades implements Interprete<EstadoTA> {
 
-    public ItemGenerator itemGen;
-    public ActividadGenerator activGen;
+    public ItemGenerator itemGen = ItemGenerator.getReference();
+    public ActividadGenerator activGen = ActividadGenerator.getReference();
     public String[] actividades;
     public String[] items;
     private ArrayList<String> l;
     private LinkedList<String> actividadesOrdenadas; // ordenadas en pre-orden
     private TreeMap<String, Set<String>> tabla;
+    private List<EstadoTA> laux;
 
-    public SistemaActividades() {
-        Item item1 = new Item("1");
-        Item item2 = new Item("2");
-        Item item3 = new Item("3");
-        Item item4 = new Item("4");
-        Item item5 = new Item("5");
-        Item item6 = new Item("6");
-        itemGen = ItemGenerator.getReference();
-
-       Item[] listaItem1 = {item1, item2};
-        Item[] listaItem2 = {item2, item4};
-        Item[] listaItem3 = {item3, item5};
-        Item[] listaItem4 = {item3, item4};
-
-        Actividad actividad1 = new Actividad("A1", listaItem1, new Item[0], new Item[0], new Item[0], new Item[0], new Item[0], new Item[]{item4}, new Conditions[0]);
-        Actividad actividad2 = new Actividad("A2", listaItem2, new Item[0], new Item[0], new Item[0], new Item[0], new Item[]{item4}, new Item[0], new Conditions[0]);
-        Actividad actividad3 = new Actividad("A3", listaItem3, new Item[0], new Item[0], new Item[0], new Item[0], new Item[0], new Item[0], new Conditions[0]);
-        Actividad actividad4 = new Actividad("A4", listaItem4, new Item[0], new Item[0], new Item[0], new Item[0], new Item[0], new Item[0], new Conditions[0]);
-        // Aqui construir el arbol de actividades
-        actividad1.addActividadHija(actividad2);
-        actividad1.addActividadHija(actividad4);
-
-        activGen = ActividadGenerator.getReference();
-        try {
-            itemGen.addItem(item1);
-            itemGen.addItem(item2);
-            itemGen.addItem(item3);
-            itemGen.addItem(item4);
-            itemGen.addItem(item5);
-            itemGen.addItem(item6);
-            activGen.addActividad(actividad1);
-            activGen.addActividad(actividad2);
-            activGen.addActividad(actividad3);
-            activGen.addActividad(actividad4);
+    public SistemaActividades(List<EstadoTA> laux) {
+            this.laux = laux;
             actividades = activGen.getConjunto().keySet().toArray(new String[0]);
             items = itemGen.getItems();
 
@@ -128,25 +97,9 @@ public class SistemaActividades implements Interprete<EstadoTA> {
                 }
                 tabla.put(a1.getNombre(), conjComp);
             }
-
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-
     }
 
     public List<EstadoTA> iniciales() {
-        ListaEstadosActividades lEstAct = new ListaEstadosActividades();
-        for (int i = 0; i < actividades.length; i++) {
-            lEstAct.addEstado(actividades[i], EstadoActividad.Waiting);
-        }
-        ListaEstadosItems lEstItems = new ListaEstadosItems();
-        for (int i = 0; i < items.length; i++) {
-            lEstItems.addEstado(items[i], EstadoItem.FREE);
-        }
-        EstadoTA estadoIni = new EstadoTA(lEstItems, lEstAct, new TreeMap<String, Set<String>>());
-        List<EstadoTA> laux = new ArrayList<EstadoTA>();
-        laux.add(estadoIni);
         return laux;
     }
 
